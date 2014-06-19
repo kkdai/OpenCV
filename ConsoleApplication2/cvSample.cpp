7c3fb87975a1daa176699e775f42cc197f726aa1
Evan Lin <Evan_Lin@chicony.com.tw>
2014-06-19 16:59:18 +0800

    Add openCV mouse control to handle camera image and crop it out

--- ConsoleApplication2/cvSample.cpp
+++ ConsoleApplication2/cvSample.cpp
@@ -13,6 +13,68 @@
 using namespace std;
 using namespace cv;
 
+static bool drag_start = false;
+static bool rectangle_on = false;
+static int  Pos_x = 0;
+static int  Pos_y = 0;
+
+static int  Sel_x = 0;
+static int  Sel_y = 0;
+
+IplImage *frame = NULL;
+IplImage *gray = NULL;
+IplImage *tracking = NULL;
+
+static int minPosX = 0;
+static int minPosY = 0;
+static int maxPosX = 0;
+static int maxPosY = 0;
+
+static void onMouse(int event, int x, int y, int, void*)
+{
+	if (event == EVENT_LBUTTONDOWN) {
+		drag_start = true;
+		Pos_x = x;
+		Pos_y = y;
+		minPosX = 0; minPosY = 0; maxPosX = 0; maxPosY = 0;
+		rectangle_on = false;
+	}
+	else if (event == EVENT_LBUTTONUP)  {
+		//Show new image
+		cvSetImageROI(frame, cvRect(minPosX, minPosY, maxPosX - minPosX, maxPosY - minPosY));
+		printf("x=%d,y=%d,width=%d,height=%d \n", minPosX, minPosY, maxPosX - minPosX, maxPosY - minPosY);
+		CvSize cvs = cvGetSize(frame);
+		printf("size width=%d, height=%d \n", cvs.width, cvs.height);
+		tracking = cvCreateImage(cvGetSize(frame), frame->depth, frame->nChannels);
+		cvCopy(frame, tracking, NULL);
+		cvResetImageROI(frame);
+		cvDestroyWindow("Tracking Image");
+		cvShowImage("Tracking Image", tracking);
+
+		//if sel[2] > sel[0] and sel[3] > sel[1]:
+		//patch = gray[sel[1]:sel[3], sel[0] : sel[2]]
+			//result = cv.matchTemplate(gray, patch, cv.TM_CCOEFF_NORMED)
+			//result = np.abs(result)**3
+			//val, result = cv.threshold(result, 0.01, 0, cv.THRESH_TOZERO)
+			//result8 = cv.normalize(result, None, 0, 255, cv.NORM_MINMAX, cv.CV_8U)
+			//cv.imshow("result", result8)
+		rectangle_on = true;
+		drag_start = false;
+	}
+	else if (drag_start) { //Drag start
+		minPosX = min(Pos_x, x);
+		minPosY = min(Pos_y, y);
+		maxPosX = max(Pos_x, x);
+		maxPosY = max(Pos_y, y);
+		cvRectangle(frame, cvPoint(minPosX, minPosY), cvPoint(maxPosX, maxPosY), CV_RGB(255, 0, 0), 1, 8, 0);
+		cvShowImage("Webcam", frame);
+	}
+	else {
+		//printf("selection is complete");
+		drag_start = false;
+	}
+}
+
 IplImage* transposeImage(IplImage* image) {
 
 	IplImage *rotated = cvCreateImage(cvSize(image->height, image->width),
@@ -56,12 +118,12 @@ IplImage *rotate_image(IplImage *image, int _90_degrees_steps_anti_clockwise)
 int _tmain(int argc, const char** argv)
 {
 	CvCapture* capture = 0;
-	IplImage *frame;
 	capture = cvCaptureFromCAM(0); //0=default, -1=any camera, 1..99=your camera
 	if (!capture) cout << "No camera detected" << endl;
 
 	cvNamedWindow("Webcam", 1);
-	
+	setMouseCallback("Webcam", onMouse, 0);
+
 	CvVideoWriter *writer;
 	char AviFileName[] = "Output.avi";
 	int AviForamt = -1;
@@ -76,6 +138,9 @@ int _tmain(int argc, const char** argv)
 		frame = cvQueryFrame(capture);
 		//cvWriteFrame(writer, frame);
 
+		//cv::cvtColor(frame, gray, COLOR_BGR2GRAY);
+		if (rectangle_on)
+			cvRectangle(frame, cvPoint(minPosX, minPosY), cvPoint(maxPosX, maxPosY), CV_RGB(255, 0, 0), 1, 8, 0);
 		cvShowImage("Webcam", frame);
 		//printf("%d\n", i);
 
